bf477f7a7f0842c377ebe6487298d59e7389f260
bohuai <tommy19900218@gmail.com>
2020-04-05 14:06:35 -0400

    LoginTest

---
+++ src/test/java/com/weborders/tests/LoginTest.java
@@ -0,0 +1,12 @@
+package com.weborders.tests;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * @author:
+ * @create:
+ * @date:
+ */
+public class LoginTest extends AbstractBaseTest {
+
+}
